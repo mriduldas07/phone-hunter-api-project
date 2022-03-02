4a2c18ddd048c5b7e3e431ef5395b80b209e31e4
mriduldas07 <mriduldas0325@gmail.com>
2022-03-02 16:53:55 +0600

    final

--- js/script.js
+++ js/script.js
@@ -1,41 +1,53 @@
+
 //search btn
 const searchPhone = () =>{
   const searchText = document.getElementById('search-input').value;
+  const searchTextNew = searchText.toLowerCase();
     // spinnerFunction call
     toggleSpinner('block')
     //clear search
     document.getElementById('search-input').value = '';
-    //fetch phone api
+    if (searchText === '') {
+      displayError('block');
+    }
+    else if(searchTextNew) {
+        //fetch phone api
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     fetch(url)
     .then(res => res.json())
     .then(data => displayPhone(data.data))
+  }  
+}
+// error display on UI
+const displayError = displayError =>{
+    document.getElementById('error-result').style.display = displayError;
 }
 // display phone
 const displayPhone = phones =>{
-    const searchResult = document.getElementById('display-phone');
-    document.getElementById('display-phone').innerHTML = '';
-    document.getElementById('display-details').innerHTML = '';
-    phones.slice(0,20).forEach(phone => {
-        const div = document.createElement('div');
-        div.classList.add('row');
-        div.innerHTML = `
-        <div class="col">
-        <div class="card mt-2 shadow-lg border border-info rounded">
-          <img style="width:150px;" src="${phone.image}" class="card-img-top m-auto pt-2 " alt="...">
-          <div class="card-body">
-            <h4 class="card-title">${phone.phone_name} </h4>
-            <h5 class="card-text">Brand: ${phone.brand} </h5>
-            <button onclick="exploreDetailes('${phone.slug}')" type="button" class="btn btn-outline-info fw-bold m-3">Explore...</button>
+      const searchResult = document.getElementById('display-phone');
+      document.getElementById('display-phone').textContent = '';
+      document.getElementById('display-details').textContent = '';
+      phones.slice(0,20).forEach(phone => {
+          const div = document.createElement('div');
+          div.classList.add('row');
+          div.innerHTML = `
+          <div class="col">
+          <div class="card mt-2 shadow-lg border border-info rounded">
+            <img style="width:150px;" src="${phone.image}" class="card-img-top m-auto pt-2 " alt="...">
+            <div class="card-body">
+              <h4 class="card-title">${phone.phone_name} </h4>
+              <h5 class="card-text">Brand: ${phone.brand} </h5>
+              <button onclick="exploreDetailes('${phone.slug}')" type="button" class="btn btn-outline-info fw-bold m-3">Explore...</button>
+            </div>
           </div>
         </div>
-      </div>
-        `;
-        searchResult.appendChild(div);
-    });
-    //spinnerFunction call
-    toggleSpinner('none')
-}
+          `;
+          searchResult.appendChild(div);
+      });
+      //spinnerFunction call
+      toggleSpinner('none')
+      displayError('none')
+    }
 // exploring phone details
 const exploreDetailes = phoneId =>{
     const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`
@@ -46,7 +58,7 @@ const exploreDetailes = phoneId =>{
 //display phone detailes
 const displayDetails = (details) =>{
     const detailsResult = document.getElementById('display-details');
-    document.getElementById('display-details').innerHTML ='';
+    document.getElementById('display-details').textContent ='';
     const div = document.createElement('div');
     div.classList.add('row');
     div.innerHTML = `
